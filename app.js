2055418c1ca2cb1721fc44c31c70a2a9f06e68d6
Артур Ковцун <Kovtsun-1471@kpnu.km.ua>
2017-09-22 12:44:29 +0300

    fixed write Algo Demo2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function writeAlgoDemo1() {
+﻿function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("<p>Side a=" + side_a + "Side b=" + side_b + "</p>");
@@ -8,9 +8,9 @@ function writeAlgoDemo1() {
 }
 
 function writeAlgoDemo2() {
-    var side_a = math.random() * 10 + 5;
-    var side_a = math.random() * 10 + 5;
-    document.write("<p>Side a= " + side_a + "</p><p>Side b=" + side_b + "</p>");
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
+    document.write("<p>Side a= " + side_a + "</p> <p>Side b=" + side_b + "</p>");
     document.write("<p>Площадь прямогульного треугольника " + side_a * side_b / 2 + "</p>");
 
 }
