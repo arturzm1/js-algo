f3b5a686fda190d543e30812ae6fd75aecb54aa9
Артур Ковцун <Kovtsun-1471@kpnu.km.ua>
2017-09-22 12:02:50 +0300

    Changes (view) in writeAlgoDemo

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ function writeAlgoDemo() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("Side a=" + side_a + "Side b=" + side_b);
+    document.writeln();
     document.write("обьем куба" + side_a * side_a * side_b);
+    document.writeln();
     document.write("Площидь квадрата" + side_a * side_a);
+    document.writeln();
     document.write("Площадь прямоугольника" + side_a * side_b);
+    document.writeln();
 }
