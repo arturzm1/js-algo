8191a8849078866f33d28055cac0dcb4f1a7b5cc
Артур Ковцун <Kovtsun-1471@kpnu.km.ua>
2017-09-22 12:19:37 +0300

    Added algo for triangle

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("Side a=" + side_a + "Side b=" + side_b);
-    document.writeln();
     document.write("обьем куба" + side_a * side_a * side_b);
-    document.writeln();
     document.write("Площидь квадрата" + side_a * side_a);
-    document.writeln();
     document.write("Площадь прямоугольника" + side_a * side_b);
-    document.writeln();
 }
 
 function writeAlgoDemo2() {
+    var side_a = math.random() * 10 + 5;
+    var side_a = math.random() * 10 + 5;
+    document.write("<p>Side a= " + side_a + "</p><p>Side b=" + side_b + "</p>");
+    document.write("<p>Площадь прямогульного треугольника " + side_a * side_b / 2 + "</p>");
 
 }
