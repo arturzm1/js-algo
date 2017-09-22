3f1326b7f2de6668d81d8bff14d70edf3ab194d7
Артур Ковцун <Kovtsun-1471@kpnu.km.ua>
2017-09-22 12:44:29 +0300

    git
    Signed-off-by: Артур Ковцун <Kovtsun-1471@kpnu.km.ua>

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ function writeAlgoDemo1() {
 function writeAlgoDemo2() {
     var side_a = math.random() * 10 + 5;
     var side_a = math.random() * 10 + 5;
-    document.write("<p>Side a= " + side_a + "</p><p>Side b=" + side_b + "</p>");
-    document.write("<p>Площадь прямогульного треугольника " + side_a * side_b / 2 + "</p>");
+    document.write("sssss");
+    //document.write("<p>Side a= " + side_a + "</p> <p>Side b=" + side_b + "</p>");
+    //document.write("<p>Площадь прямогульного треугольника " + side_a * side_b / 2 + "</p>");
+    //document.write("asss");
 
 }
