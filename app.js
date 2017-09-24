260a64f9b4e02a69a88ac7456ac0becb0191019d
arturzm1 <kovtsun@kpnu.km.ua>
2017-09-24 09:20:46 +0300

    fixed js

--- app.js
+++ app.js
@@ -8,18 +8,9 @@
 }
 
 function writeAlgoDemo2() {
-<<<<<<< HEAD
+
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("<p>Side a= " + side_a + "</p> <p>Side b=" + side_b + "</p>");
     document.write("<p>Площадь прямогульного треугольника " + side_a * side_b / 2 + "</p>");
-=======
-    var side_a = math.random() * 10 + 5;
-    var side_a = math.random() * 10 + 5;
-    document.write("sssss");
-    //document.write("<p>Side a= " + side_a + "</p> <p>Side b=" + side_b + "</p>");
-    //document.write("<p>Площадь прямогульного треугольника " + side_a * side_b / 2 + "</p>");
-    //document.write("asss");
->>>>>>> 3f1326b7f2de6668d81d8bff14d70edf3ab194d7
-
 }
